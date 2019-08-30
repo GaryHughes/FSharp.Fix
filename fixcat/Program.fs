31423224f35336cc9de518bb1b9797f2e8e56473
Gary Hughes <garyedwardhughes@gmail.com>
2019-08-30 17:58:37 +1000

    Read from stdin or named files.

--- fixcat/Program.fs
+++ fixcat/Program.fs
@@ -1,18 +1,28 @@
 ﻿open System
+open System.IO
 open FSharp.Fix
+
+let catStream stream = 
+    parseMessagesFromLog stream
+        |> Seq.map(fun result -> 
+            match result with 
+            | Ok message -> Some message
+            | Error _ -> None)
+        |> Seq.choose id
+        |> Seq.map prettyPrint
+        |> Seq.iter Console.WriteLine
 
 [<EntryPoint>]
 let main argv =
 
-    use stream = Console.OpenStandardInput()
-   
-    parseMessagesFromLog stream
-    |> Seq.map(fun result -> 
-        match result with 
-        | Ok message -> Some message
-        | Error _ -> None)
-    |> Seq.choose id
-    |> Seq.map prettyPrint
-    |> Seq.iter Console.WriteLine
+    if argv.Length = 0 then
+        use stream = Console.OpenStandardInput()
+        catStream stream
+    else
+        argv
+        |> Seq.iter(fun filename ->
+            use stream = File.OpenRead(filename)
+            catStream stream
+        )
     
     0 // return an integer exit code
